2a692174ef67c94270f4825ebbbe9992b04fe4fc
Cristhian Santos <cristhianfelipe0@gmail.com>
2023-06-01 18:08:55 -0300

    🚧 chore: test pipeline

--- extension-youtube-data-analyze/src/main.ts
+++ extension-youtube-data-analyze/src/main.ts
@@ -2,6 +2,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+//AIzaSyABvsKu6o9LAdsFM3IqbK-_eKz7ZxdCN_Y
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
